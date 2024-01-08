ebb84698244d13ec42cb8392e51e2a271ea05280
ndigvijay <digvijayn1993@gmail.com>
2024-01-08 14:19:03 +0530

    vercel.json

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 4000;
+const routes = require("./Routes/routes");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config(); 
+
+app.use(express.json());
+app.use(cors());
+app.use("/app", routes);
+
+const mongoURI = process.env.MONGO_URI;
+
+
+
+try {
+  mongoose.connect(mongoURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+  });
+  console.log("MongoDB connected successfully");
+} catch (error) {
+  console.error("Error connecting to MongoDB:",error);
+}
+
+
+app.listen(PORT, () => {
+  console.log("Server is running");
+});
